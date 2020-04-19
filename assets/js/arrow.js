c22a42edf46133d19fcc6633aadfb709a619ce0a
xzqu1t <xzqu1t@users.noreply.github.com>
2020-04-19 21:59:18 +0300

    index9

--- assets/js/arrow.js
+++
@@ -1,8 +0,0 @@
-    arrowTop.onclick = function() {
-        window.scrollTo(pageXOffset, 0);
-        // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
-      };
-  
-      window.addEventListener('scroll', function() {
-        arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
-      });
